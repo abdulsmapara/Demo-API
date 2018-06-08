c224b353ad1876d143e01b59c45dbc018266878e
ABDUL SATTAR MAPARA <32198670+abdulsmapara@users.noreply.github.com>
2018-06-08 14:57:52 +0530

    Update dbconnection.js

--- dbconnection.js
+++ dbconnection.js
@@ -2,7 +2,7 @@ var mysql=require('mysql');
 var connection=mysql.createPool({
 	host: 'localhost',
 	user: 'root',
-	password: 'Mapara',
+	password: 'password',
 	database: 'demo'
 });
 module.exports=connection;
